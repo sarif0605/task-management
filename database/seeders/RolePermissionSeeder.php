4ca58fd407ec2afaf6766d94cb603091f2f7eef4
sarif0605 <rivzalchantara13@gmail.com>
2024-10-24 11:58:18 +0700

    setup laravel dan spatie

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+}
